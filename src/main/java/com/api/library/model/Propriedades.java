5c83400981c8c17f09f46e52f91b1bb89491cc73
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 07:42:05 -0300

    Criando modelo de dados de propriedades
    A entidade propriedades ficara responsável por conter informações sobre estruturas física do livro.

---
+++ src/main/java/com/api/library/model/Propriedades.java
@@ -0,0 +1,46 @@
+package com.api.library.model;
+
+public class Propriedades {
+
+	private Long id;
+	private Integer page;
+	private Integer isbn;
+	private String theme;
+
+	public Propriedades() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+	public Integer getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+}
