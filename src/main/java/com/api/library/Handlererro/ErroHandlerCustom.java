4c9f59d1e3055f9d1b30c1dfc9560617fd20728f
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 10:05:42 -0300

    Formatando retorno de erros corretamente.
    Definido valores e parametros padronizados de erros, campos que facilitem o entendimento e correção do erro para o usuario.

---
+++ src/main/java/com/api/library/Handlererro/ErroHandlerCustom.java
@@ -0,0 +1,27 @@
+package com.api.library.Handlererro;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.api.library.exceptions.ReturnErroFindNotFound;
+import com.api.library.modelErroResponse.modelErroReturn;
+
+@RestControllerAdvice
+public class ErroHandlerCustom {
+    
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(ReturnErroFindNotFound.class)
+	public modelErroReturn returnErroCustom(ReturnErroFindNotFound ex, WebRequest request) {
+		modelErroReturn error = new modelErroReturn(
+				HttpStatus.NOT_FOUND.value(),
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+				return error;	
+	}
+}
