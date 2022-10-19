dc1c10ccc7a9852277479c88440082961fe45601
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 07:45:16 -0300

    Criando modelo de dados e publicacao
    Sera responsavel por conter informações sobre a editora e métricas pre estabelecida.

---
+++ src/main/java/com/api/library/model/Publicacao.java
@@ -0,0 +1,49 @@
+package com.api.library.model;
+
+import java.util.Date;
+
+public class Publicacao {
+  
+	private Long id;
+	private String coverType;
+	private Date publicationDate;
+	private String dimensoes;
+	
+	public Publicacao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCoverType() {
+		return coverType;
+	}
+
+	public void setCoverType(String coverType) {
+		this.coverType = coverType;
+	}
+
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+
+	public String getDimensoes() {
+		return dimensoes;
+	}
+
+	public void setDimensoes(String dimensoes) {
+		this.dimensoes = dimensoes;
+	}
+	
+	
+}
