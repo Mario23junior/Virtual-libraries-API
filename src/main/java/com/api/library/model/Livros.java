8a570b47b9b2fbf362d45e0dfd80c5fcd6ffe284
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 07:49:52 -0300

    Definindo a classe como mapeadora
    Definindo classe como mapeadora de dados para o banco de dados.

--- src/main/java/com/api/library/model/Livros.java
+++ src/main/java/com/api/library/model/Livros.java
@@ -1,12 +1,30 @@
 package com.api.library.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Livros {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String image;
+	
+	@Column(nullable = false)
 	private String name;
+	
+	@Column(nullable = false)
 	private String author;
+	
 	private Integer edition;
+	
+	@Column(nullable = false)
 	private String PublishingCompany;
 	
 	public Livros() {
