553f7ba8d912d6165289db9090e2d779dee2e0cb
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 12:29:28 -0300

    Listando publicações por id de idetificacao

--- src/main/java/com/api/library/controller/PublicacaoController.java
+++ src/main/java/com/api/library/controller/PublicacaoController.java
@@ -1,6 +1,8 @@
 package com.api.library.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,10 @@ public class PublicacaoController {
 	public ResponseEntity<PublicacaoDTO> savePublic(
 			@RequestBody PublicacaoDTO publiDto){
 		return service.savePublicao(publiDto);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<PublicacaoDTO> listId(@PathVariable Long id){
+		return service.listIdPublicao(id);
 	}
 }
