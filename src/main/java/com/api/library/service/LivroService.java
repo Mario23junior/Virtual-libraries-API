8e06463d98fdf42cb81cb68e7146d3f31d138d17
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 09:34:14 -0300

    Atualizando informacoes de livros por ID

--- src/main/java/com/api/library/service/LivroService.java
+++ src/main/java/com/api/library/service/LivroService.java
@@ -26,25 +26,39 @@ public class LivroService {
 	public ResponseEntity<LivrosDTO> saveLivro(LivrosDTO livroDto) {
 		try {
 			Livros livroSave = bodySave(mapper.map(livroDto, Livros.class));
-			return ResponseEntity
-					.status(HttpStatus.OK)
-					.body(mapper.map(livroSave, LivrosDTO.class));
-		}catch(Exception i) {
-		      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+			return ResponseEntity.status(HttpStatus.OK).body(mapper.map(livroSave, LivrosDTO.class));
+		} catch (Exception i) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
- 			
+
 	}
 
 	private Livros bodySave(Livros livro) {
 		return repository.save(livro);
 	}
-	
-	public ResponseEntity<LivrosDTO> listId(Long id){
+
+	public ResponseEntity<LivrosDTO> listId(Long id) {
 		Optional<Livros> livroId = repository.findById(id);
-		if(livroId.isPresent()) {
-			return ResponseEntity.ok(mapper.map(livroId.get(),LivrosDTO.class));			
+		if (livroId.isPresent()) {
+			return ResponseEntity.ok(mapper.map(livroId.get(), LivrosDTO.class));
 		} else {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);			
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
+
+	public ResponseEntity<LivrosDTO> updateLivro(LivrosDTO livroDto, Long id) {
+		Optional<Livros> livrosId = repository.findById(id);
+		if (livrosId.isPresent()) {
+			Livros livroUpdate = livrosId.get();
+			livroUpdate.setImage(livroDto.getImage());
+			livroUpdate.setName(livroDto.getName());
+			livroUpdate.setAuthor(livroDto.getAuthor());
+			livroUpdate.setEdition(livroDto.getEdition());
+			livroUpdate.setPublishingCompany(livroDto.getPublishingCompany());
+			repository.save(livroUpdate);
+			return ResponseEntity.ok(mapper.map(livroUpdate, LivrosDTO.class));
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 	}
 
