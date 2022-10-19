b35f1eb543fa3db91d4afd13ec9732bc48312056
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-19 08:09:00 -0300

    Criando repositorys da entidade Publicacao.

---
+++ src/main/java/com/api/library/repository/PublicacaoRepository.java
@@ -0,0 +1,9 @@
+package com.api.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.library.model.Publicacao;
+
+public interface PublicacaoRepository extends JpaRepository<Publicacao, Long>{
+
+}
